fd33ea813ece445baceac9d30ed2ab3fe02d4bfb
s21sd <srivastavasunny359@gmail.com>
2024-03-29 22:12:36 +0530

    dd

--- animated-threejs-toast/src/Component/Toastcontaier.jsx
+++ animated-threejs-toast/src/Component/Toastcontaier.jsx
@@ -4,7 +4,8 @@ import { BiSolidError } from "react-icons/bi";
 import { MdError } from "react-icons/md";
 import { CiCircleInfo } from "react-icons/ci";
 import { FaXmark } from "react-icons/fa6";
-
+import { motion } from 'framer-motion'
+import { transition1 } from './Transition';
 const ToastContainer = ({ triggerSuccessToast }) => {
 
     const [toasts, setToasts] = useState([]);
@@ -74,10 +75,18 @@ const ToastContainer = ({ triggerSuccessToast }) => {
         setTimeout(() => removeToast(newToast.id), 5000);
     };
     return (
-        <div>
-            <div className="notifications" style={{ position: 'fixed', top: '30px', right: '20px' }}>
+        <motion.div>
+            <motion.div
+
+                className="notifications" style={{ position: 'fixed', top: '30px', right: '20px' }}>
                 {toasts.map((toast, index) => (
-                    <div key={index} className={`toast ${toast.id}`} style={{ width: '400px', position: 'relative', overflow: 'hidden', listStyle: 'none', borderRadius: '4px', padding: '16px 17px', marginBottom: '10px', background: toast.color, justifyContent: 'space-between', animation: 'show_toast 0.3s ease forwards' }}>
+
+                    <motion.div
+                        initial={{ opacity: 0, y: '-50%' }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: '-50%' }}
+                        transition={{ type: 'spring', stiffness: 200, damping: 10 }}
+                        key={index} className={`toast ${toast.id}`} style={{ width: '400px', position: 'relative', overflow: 'hidden', listStyle: 'none', borderRadius: '4px', padding: '16px 17px', marginBottom: '10px', background: toast.color, justifyContent: 'space-between', animation: 'show_toast 0.3s ease forwards' }}>
                         <div className="column" style={{ display: 'flex', alignItems: 'center' }}>
                             <div>
                                 {toast.icon}
@@ -86,16 +95,16 @@ const ToastContainer = ({ triggerSuccessToast }) => {
                         </div>
                         <FaXmark size={20} className="fa-solid fa-xmark" style={{ color: 'white', cursor: 'pointer' }} onClick={() => removeToast(toast.id)} />
                         <div style={{ position: 'absolute', content: '', height: '4px', width: '100%', bottom: '0px', left: '0px', animation: 'progress 5s linear forwards', background: 'var(--light)' }}></div>
-                    </div>
+                    </motion.div>
                 ))}
-            </div>
-            <div className="buttons">
+            </motion.div>
+            <motion.div className="buttons">
                 <button className="btn" id="success" onClick={() => createToast('success')}>Success</button>
                 <button className="btn" id="error" onClick={() => createToast('error')}>Error</button>
                 <button className="btn" id="warning" onClick={() => createToast('warning')}>Warning</button>
                 <button className="btn" id="info" onClick={() => createToast('info')}>Info</button>
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     );
 };
 
